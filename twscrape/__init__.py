4b70261cfaa1bc445111ecbaaa2b3957057a5658
Vlad Pronsky <v.pronsky@gmail.com>
2024-04-18 04:31:54 +0300

    add NoAccountError as top level import

--- twscrape/__init__.py
+++ twscrape/__init__.py
@@ -1,6 +1,6 @@
 # ruff: noqa: F401
 from .account import Account
-from .accounts_pool import AccountsPool
+from .accounts_pool import AccountsPool, NoAccountError
 from .api import API
 from .logger import set_log_level
 from .models import *  # noqa: F403
