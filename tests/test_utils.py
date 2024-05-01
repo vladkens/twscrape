81d16ee6c397f657d12ce8ab53e76a3c24a03781
crisnelio551 <168581237+crisnelio551@users.noreply.github.com>
2024-05-01 19:14:26 +0000

    improve

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,27 +1,6 @@
 import pytest
+import json
+import re
 
 from twscrape.utils import parse_cookies
 
-
-def test_cookies_parse():
-    val = "abc=123; def=456; ghi=789"
-    assert parse_cookies(val) == {"abc": "123", "def": "456", "ghi": "789"}
-
-    val = '{"abc": "123", "def": "456", "ghi": "789"}'
-    assert parse_cookies(val) == {"abc": "123", "def": "456", "ghi": "789"}
-
-    val = '[{"name": "abc", "value": "123"}, {"name": "def", "value": "456"}, {"name": "ghi", "value": "789"}]'
-    assert parse_cookies(val) == {"abc": "123", "def": "456", "ghi": "789"}
-
-    val = "eyJhYmMiOiAiMTIzIiwgImRlZiI6ICI0NTYiLCAiZ2hpIjogIjc4OSJ9"
-    assert parse_cookies(val) == {"abc": "123", "def": "456", "ghi": "789"}
-
-    val = "W3sibmFtZSI6ICJhYmMiLCAidmFsdWUiOiAiMTIzIn0sIHsibmFtZSI6ICJkZWYiLCAidmFsdWUiOiAiNDU2In0sIHsibmFtZSI6ICJnaGkiLCAidmFsdWUiOiAiNzg5In1d"
-    assert parse_cookies(val) == {"abc": "123", "def": "456", "ghi": "789"}
-
-    val = '{"cookies": {"abc": "123", "def": "456", "ghi": "789"}}'
-    assert parse_cookies(val) == {"abc": "123", "def": "456", "ghi": "789"}
-
-    with pytest.raises(ValueError, match=r"Invalid cookie value: .+"):
-        val = "{invalid}"
-        assert parse_cookies(val) == {}
