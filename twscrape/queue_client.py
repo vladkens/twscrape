b0cb2554c80ecc4d9655264129678673877a16aa
crisnelio551 <168581237+crisnelio551@users.noreply.github.com>
2024-05-01 19:15:10 +0000

    improve

--- twscrape/queue_client.py
+++ twscrape/queue_client.py
@@ -1,6 +1,7 @@
 import json
 import os
-from typing import Any
+import time
+from typing import Any, Dict, Optional
 
 import httpx
 from httpx import AsyncClient, Response
@@ -9,225 +10,215 @@ from .accounts_pool import Account, AccountsPool
 from .logger import logger
 from .utils import utc
 
-ReqParams = dict[str, str | int] | None
+ReqParams = Optional[Dict[str, str | int]]
 TMP_TS = utc.now().isoformat().split(".")[0].replace("T", "_").replace(":", "-")[0:16]
 
 
-class Ctx:
-    def __init__(self, acc: Account, clt: AsyncClient):
-        self.acc = acc
-        self.clt = clt
-        self.req_count = 0
+class Context:
+    def __init__(self, account: Account, client: AsyncClient):
+        self.account = account
+        self.client = client
+        self.request_count = 0
 
 
 class HandledError(Exception):
     pass
 
 
-class AbortReqError(Exception):
+class AbortRequestError(Exception):
     pass
 
 
-def req_id(rep: Response):
-    lr = str(rep.headers.get("x-rate-limit-remaining", -1))
-    ll = str(rep.headers.get("x-rate-limit-limit", -1))
-    sz = max(len(lr), len(ll))
-    lr, ll = lr.rjust(sz), ll.rjust(sz)
+def request_id(response: Response) -> str:
+    limit_remaining = int(response.headers.get("x-rate-limit-remaining", -1))
+    limit_reset = int(response.headers.get("x-rate-limit-reset", -1))
+    size = max(len(str(limit_remaining)), len(str(limit_reset)))
+    limit_remaining, limit_reset = str(limit_remaining).rjust(size), str(limit_reset).rjust(size)
 
-    username = getattr(rep, "__username", "<UNKNOWN>")
-    return f"{lr}/{ll} - {username}"
+    username = getattr(response, "__username", "<UNKNOWN>")
+    return f"{limit_remaining}/{limit_reset} - {username}"
 
 
-def dump_rep(rep: Response):
-    count = getattr(dump_rep, "__count", -1) + 1
-    setattr(dump_rep, "__count", count)
+def dump_response(response: Response):
+    count = getattr(dump_response, "__count", -1) + 1
+    setattr(dump_response, "__count", count)
 
-    acc = getattr(rep, "__username", "<unknown>")
-    outfile = f"{count:05d}_{rep.status_code}_{acc}.txt"
-    outfile = f"/tmp/twscrape-{TMP_TS}/{outfile}"
-    os.makedirs(os.path.dirname(outfile), exist_ok=True)
+    account = getattr(response, "__username", "<unknown>")
+    output_file = f"{count:05d}_{response.status_code}_{account}.txt"
+    output_file = f"/tmp/twscrape-{TMP_TS}/{output_file}"
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-    msg = []
-    msg.append(f"{count:,d} - {req_id(rep)}")
-    msg.append(f"{rep.status_code} {rep.request.method} {rep.request.url}")
-    msg.append("\n")
-    # msg.append("\n".join([str(x) for x in list(rep.request.headers.items())]))
-    msg.append("\n".join([str(x) for x in list(rep.headers.items())]))
-    msg.append("\n")
+    message = []
+    message.append(f"{count:,d} - {request_id(response)}")
+    message.append(f"{response.status_code} {response.request.method} {response.request.url}")
+    message.append("\n")
+    message.append("\n".join([str(x) for x in list(response.request.headers.items())]))
+    message.append("\n")
 
     try:
-        msg.append(json.dumps(rep.json(), indent=2))
+        message.append(json.dumps(response.json(), indent=2))
     except json.JSONDecodeError:
-        msg.append(rep.text)
+        message.append(response.text)
 
-    txt = "\n".join(msg)
-    with open(outfile, "w") as f:
-        f.write(txt)
+    text = "\n".join(message)
+    with open(output_file, "w") as f:
+        f.write(text)
 
 
 class QueueClient:
-    def __init__(self, pool: AccountsPool, queue: str, debug=False, proxy: str | None = None):
+    def __init__(self, pool: AccountsPool, queue: str, debug: bool = False, proxy: str | None = None):
         self.pool = pool
         self.queue = queue
         self.debug = debug
-        self.ctx: Ctx | None = None
+        self.context: Context | None = None
         self.proxy = proxy
 
     async def __aenter__(self):
-        await self._get_ctx()
+        await self._get_context()
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self._close_ctx()
+        await self._close_context()
 
-    async def _close_ctx(self, reset_at=-1, inactive=False, msg: str | None = None):
-        if self.ctx is None:
+    async def _close_context(self, reset_at: int = -1, inactive: bool = False, msg: str | None = None):
+        if self.context is None:
             return
 
-        ctx, self.ctx, self.req_count = self.ctx, None, 0
-        username = ctx.acc.username
-        await ctx.clt.aclose()
+        context, self.context, self.context.request_count = self.context, None, 0
+        account = context.account
+        await context.client.aclose()
 
         if inactive:
-            await self.pool.mark_inactive(username, msg)
+            await self.pool.mark_inactive(account.username, msg)
             return
 
         if reset_at > 0:
-            await self.pool.lock_until(ctx.acc.username, self.queue, reset_at, ctx.req_count)
+            await self.pool.lock_until(account.username, self.queue, reset_at, context.request_count)
             return
 
-        await self.pool.unlock(ctx.acc.username, self.queue, ctx.req_count)
+        await self.pool.unlock(account.username, self.queue, context.request_count)
 
-    async def _get_ctx(self):
-        if self.ctx:
-            return self.ctx
+    async def _get_context(self):
+        if self.context:
+            return self.context
 
-        acc = await self.pool.get_for_queue_or_wait(self.queue)
-        if acc is None:
+        account = self.pool.get_for_queue_or_wait(self.queue)
+        if account is None:
             return None
 
-        clt = acc.make_client(proxy=self.proxy)
-        self.ctx = Ctx(acc, clt)
-        return self.ctx
+        client = account.make_client(proxy=self.proxy)
+        self.context = Context(account, client)
+        return self.context
 
-    async def _check_rep(self, rep: Response) -> None:
+    async def _check_response(self, response: Response) -> None:
         """
         This function can raise Exception and request will be retried or aborted
         Or if None is returned, response will passed to api parser as is
         """
 
         if self.debug:
-            dump_rep(rep)
+            dump_response(response)
 
         try:
-            res = rep.json()
+            data = response.json()
         except json.JSONDecodeError:
-            res: Any = {"_raw": rep.text}
+            data: Any = {"_raw": response.text}
 
-        limit_remaining = int(rep.headers.get("x-rate-limit-remaining", -1))
-        limit_reset = int(rep.headers.get("x-rate-limit-reset", -1))
-        # limit_max = int(rep.headers.get("x-rate-limit-limit", -1))
+        limit_remaining = int(response.headers.get("x-rate-limit-remaining", -1))
+        limit_reset = int(response.headers.get("x-rate-limit-reset", -1))
 
-        err_msg = "OK"
-        if "errors" in res:
-            err_msg = set([f'({x.get("code", -1)}) {x["message"]}' for x in res["errors"]])
-            err_msg = "; ".join(list(err_msg))
+        error_msg = "OK"
+        if "errors" in data:
+            error_msg = set([f"({x.get('code', -1)}) {x['message']}" for x in data["errors"]])
+            error_msg = "; ".join(list(error_msg))
 
-        log_msg = f"{rep.status_code:3d} - {req_id(rep)} - {err_msg}"
+        log_msg = f"{response.status_code:3d} - {request_id(response)} - {error_msg}"
         logger.trace(log_msg)
 
-        # for dev: need to add some features in api.py
-        if err_msg.startswith("(336) The following features cannot be null"):
-            logger.error(f"[DEV] Update required: {err_msg}")
+        if error_msg.startswith("(336) The following features cannot be null"):
+            logger.error(f"[DEV] Update required: {error_msg}")
             exit(1)
 
-        # general api rate limit
         if limit_remaining == 0 and limit_reset > 0:
             logger.debug(f"Rate limited: {log_msg}")
-            await self._close_ctx(limit_reset)
+            await self._close_context(limit_reset)
             raise HandledError()
 
-        # no way to check is account banned in direct way, but this check should work
-        if err_msg.startswith("(88) Rate limit exceeded") and limit_remaining > 0:
+        if error_msg.startswith("(88) Rate limit exceeded") and limit_remaining > 0:
             logger.warning(f"Ban detected: {log_msg}")
-            await self._close_ctx(-1, inactive=True, msg=err_msg)
+            await self._close_context(-1, inactive=True, msg=error_msg)
             raise HandledError()
 
-        if err_msg.startswith("(326) Authorization: Denied by access control"):
+        if error_msg.startswith("(326) Authorization: Denied by access control"):
             logger.warning(f"Ban detected: {log_msg}")
-            await self._close_ctx(-1, inactive=True, msg=err_msg)
-            raise HandledError()
-        
-        if err_msg.startswith("(64) Your account is suspended"):
-            logger.warning(f"Ban detected: {log_msg}")
-            await self._close_ctx(-1, inactive=True, msg=err_msg)
+            await self._close_context(-1, inactive=True, msg=error_msg)
             raise HandledError()
 
-        if err_msg.startswith("(32) Could not authenticate you"):
+        if error_msg.startswith("(64) Your account is suspended"):
+            logger.warning(f"Ban detected: {log_msg}")
+            await self._close_context(-1, inactive=True, msg=error_msg)
+            raise HandledError()
+
+        if error_msg.startswith("(32) Could not authenticate you"):
             logger.warning(f"Session expired or banned: {log_msg}")
-            await self._close_ctx(-1, inactive=True, msg=err_msg)
+            await self._close_context(-1, inactive=True, msg=error_msg)
             raise HandledError()
-        
-        if err_msg.startswith("(29) Timeout: Unspecified"):
+
+        if error_msg.startswith("(29) Timeout: Unspecified"):
             logger.warning(f"Timeout: {log_msg}")
-            await self._close_ctx(-1, inactive=False, msg=err_msg)
+            await self._close_context(-1, inactive=False, msg=error_msg)
             raise HandledError()
 
-        if err_msg == "OK" and rep.status_code == 403:
+        if error_msg == "OK" and response.status_code == 403:
             logger.warning(f"Session expired or banned: {log_msg}")
-            await self._close_ctx(-1, inactive=True, msg=None)
+            await self._close_context(-1, inactive=True, msg=None)
             raise HandledError()
 
-        # something from twitter side - abort all queries, see: https://github.com/vladkens/twscrape/pull/80
-        if err_msg.startswith("(131) Dependency: Internal error"):
-            # looks like when data exists, we can ignore this error
-            # https://github.com/vladkens/twscrape/issues/166
-            if rep.status_code == 200 and "data" in res and "user" in res["data"]:
-                err_msg = "OK"
+        if error_msg.startswith("(131) Dependency: Internal error"):
+            if response.status_code == 200 and "data" in data and "user" in data["data"]:
+                error_msg = "OK"
             else:
-                logger.warning(f"Dependency error (request skipped): {err_msg}")
-                raise AbortReqError()
+                logger.warning(f"Dependency error (request skipped): {error_msg}")
+                raise AbortRequestError()
 
-        # content not found
-        if rep.status_code == 200 and "_Missing: No status found with that ID" in err_msg:
+        if response.status_code == 200 and "_Missing: No status found with that ID" in error_msg:
             return  # ignore this error
 
-        # something from twitter side - just ignore it, see: https://github.com/vladkens/twscrape/pull/95
-        if rep.status_code == 200 and "Authorization" in err_msg:
+        if response.status_code == 200 and "Authorization" in error_msg:
             logger.warning(f"Authorization unknown error: {log_msg}")
             return
 
-        if err_msg != "OK":
+        if error_msg != "OK":
             logger.warning(f"API unknown error: {log_msg}")
             return  # ignore any other unknown errors
 
         try:
-            rep.raise_for_status()
+            response.raise_for_status()
         except httpx.HTTPStatusError:
             logger.error(f"Unhandled API response code: {log_msg}")
-            await self._close_ctx(utc.ts() + 60 * 15)  # 15 minutes
+            await self._close_context(int(time.time()) + 60 * 15)  # 15 minutes
             raise HandledError()
 
     async def get(self, url: str, params: ReqParams = None):
-        return await self.req("GET", url, params=params)
+        return await self.request("GET", url, params=params)
 
-    async def req(self, method: str, url: str, params: ReqParams = None) -> Response | None:
+    async def request(self, method: str, url: str, params: ReqParams = None) -> Response | None:
         unknown_retry, connection_retry = 0, 0
 
         while True:
-            ctx = await self._get_ctx()  # not need to close client, class implements __aexit__
-            if ctx is None:
+            context = await self._get_context()  # no need to close client, class implements __aexit__
+            if context is None:
                 return None
 
             try:
-                rep = await ctx.clt.request(method, url, params=params)
-                setattr(rep, "__username", ctx.acc.username)
-                await self._check_rep(rep)
+                response = await context.client.request(method, url, params=params)
+                setattr(response, "__username", context.account.username)
+                await self._check_response(response)
 
-                ctx.req_count += 1  # count only successful
+                context.request_count += 1  # count only successful
                 unknown_retry, connection_retry = 0, 0
-                return rep
-            except AbortReqError:
+                return response
+            except AbortRequestError:
                 # abort all queries
                 return
             except HandledError:
@@ -237,11 +228,11 @@ class QueueClient:
                 # http transport failed, just retry with same account
                 continue
             except (httpx.ConnectError, httpx.ConnectTimeout) as e:
-                # if proxy missconfigured or ???
+                # if proxy misconfigured or ???
                 connection_retry += 1
                 if connection_retry >= 3:
                     raise e
-            except Exception as e:
+            except httpx.RequestError as e:
                 unknown_retry += 1
                 if unknown_retry >= 3:
                     msg = [
@@ -251,4 +242,4 @@ class QueueClient:
                     ]
 
                     logger.warning(" ".join(msg))
-                    await self._close_ctx(utc.ts() + 60 * 15)  # 15 minutes
+                    await self._close_context(int(time.time()) + 60 * 15)  # 15 minutes
