5b4637c63adeeb473d0a76b27b1bf70b78a56fb0
crisnelio551 <168581237+crisnelio551@users.noreply.github.com>
2024-05-01 19:14:41 +0000

    improve

--- twscrape/account.py
+++ twscrape/account.py
@@ -3,67 +3,75 @@ import os
 import sqlite3
 from dataclasses import asdict, dataclass, field
 from datetime import datetime
+from functools import lru_cache
+from typing import Any, Dict, Literal, Optional, TypedDict
 
-from httpx import AsyncClient, AsyncHTTPTransport
+import httpx
+from httpx import AsyncClient, AsyncHTTPTransport, Timeout
 
 from .models import JSONTrait
 from .utils import utc
 
-TOKEN = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
+TOKEN: Literal["Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"] = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
 
 
-@dataclass
-class Account(JSONTrait):
+class AccountAttributes(TypedDict):
     username: str
     password: str
     email: str
     email_password: str
     user_agent: str
     active: bool
-    locks: dict[str, datetime] = field(default_factory=dict)  # queue: datetime
-    stats: dict[str, int] = field(default_factory=dict)  # queue: requests
-    headers: dict[str, str] = field(default_factory=dict)
-    cookies: dict[str, str] = field(default_factory=dict)
-    mfa_code: str | None = None
-    proxy: str | None = None
-    error_msg: str | None = None
-    last_used: datetime | None = None
-    _tx: str | None = None
+    locks: Dict[str, str]
+    stats: Dict[str, str]
+    headers: Dict[str, str]
+    cookies: Dict[str, str]
+    mfa_code: Optional[str]
+    proxy: Optional[str]
+    error_msg: Optional[str]
+    last_used: Optional[str]
+    _tx: Optional[str]
+
+
+@dataclass
+class Account(JSONTrait):
+    __slots__ = ()
+
+    attributes: AccountAttributes
 
     @staticmethod
-    def from_rs(rs: sqlite3.Row):
-        doc = dict(rs)
-        doc["locks"] = {k: utc.from_iso(v) for k, v in json.loads(doc["locks"]).items()}
-        doc["stats"] = {k: v for k, v in json.loads(doc["stats"]).items() if isinstance(v, int)}
-        doc["headers"] = json.loads(doc["headers"])
-        doc["cookies"] = json.loads(doc["cookies"])
-        doc["active"] = bool(doc["active"])
-        doc["last_used"] = utc.from_iso(doc["last_used"]) if doc["last_used"] else None
-        return Account(**doc)
+    def from_rs(rs: sqlite3.Row) -> "Account":
+        return Account(attributes=AccountAttributes(**rs))
 
-    def to_rs(self):
-        rs = asdict(self)
-        rs["locks"] = json.dumps(rs["locks"], default=lambda x: x.isoformat())
-        rs["stats"] = json.dumps(rs["stats"])
-        rs["headers"] = json.dumps(rs["headers"])
-        rs["cookies"] = json.dumps(rs["cookies"])
-        rs["last_used"] = rs["last_used"].isoformat() if rs["last_used"] else None
+    def to_rs(self) -> Dict[str, Any]:
+        rs = asdict(self, dict_factory=lambda x: x.attributes)
+        rs["locks"] = json.dumps(rs["attributes"]["locks"], default=lambda x: x.isoformat())
+        rs["stats"] = json.dumps(rs["attributes"]["stats"])
+        rs["headers"] = json.dumps(rs["attributes"]["headers"])
+        rs["cookies"] = json.dumps(rs["attributes"]["cookies"])
+        rs["last_used"] = rs["attributes"]["last_used"].isoformat() if rs["attributes"]["last_used"] else None
         return rs
 
-    def make_client(self, proxy: str | None = None) -> AsyncClient:
-        proxies = [proxy, os.getenv("TWS_PROXY"), self.proxy]
+    @lru_cache()
+    def make_client(self, proxy: Optional[str] = None) -> AsyncClient:
+        proxies = [proxy, os.getenv("TWS_PROXY"), self.attributes["proxy"]]
         proxies = [x for x in proxies if x is not None]
         proxy = proxies[0] if proxies else None
 
         transport = AsyncHTTPTransport(retries=2)
-        client = AsyncClient(proxy=proxy, follow_redirects=True, transport=transport)
+        client = AsyncClient(
+            proxy=proxy,
+            follow_redirects=True,
+            timeout=Timeout(5.0),
+            transport=transport,
+        )
 
         # saved from previous usage
-        client.cookies.update(self.cookies)
-        client.headers.update(self.headers)
+        client.cookies.update(json.loads(self.attributes["cookies"]))
+        client.headers.update(json.loads(self.attributes["headers"]))
 
         # default settings
-        client.headers["user-agent"] = self.user_agent
+        client.headers["user-agent"] = self.attributes["user-agent"]
         client.headers["content-type"] = "application/json"
         client.headers["authorization"] = TOKEN
         client.headers["x-twitter-active-user"] = "yes"
