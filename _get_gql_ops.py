51b70ca1348cbfa790c89e6f20c93f7d3f02a12c
crisnelio551 <168581237+crisnelio551@users.noreply.github.com>
2024-05-01 19:14:39 +0000

    improve

--- _get_gql_ops.py
+++ _get_gql_ops.py
@@ -1,87 +1,98 @@
 import json
 import os
 import re
+from contextlib import ExitStack
+from httpx import Client, Limits
+from urllib.parse import urlparse, urljoin
+import fake_useragent
 
-import httpx
-from fake_useragent import UserAgent
+import twscrape.api  # noqa: F401
 
-"""
-docker run --rm -p "3128:3128/tcp" -p "1080:1080/tcp" -e "PROXY_LOGIN=user" -e "PROXY_PASSWORD=pass" tarampampam/3proxy
-docker run --rm -p "3129:3128/tcp" -p "1081:1080/tcp" tarampampam/3proxy
-"""
-
-client = httpx.Client(headers={"user-agent": UserAgent().chrome})
-
-with open("./twscrape/api.py") as fp:
-    ops = [x.strip() for x in fp.read().split("\n")]
-    ops = [x.split("=")[0].removeprefix("OP_").strip() for x in ops if x.startswith("OP_")]
+REGEX_PATTERN = re.compile(r'queryId:"(?P<query_id>.+?)".+?operationName:"(?P<operation_name>.+?)"')
 
 
-def script_url(k: str, v: str):
+def script_url(k: str, v: str) -> str:
+    """Return the script URL for the given key and version."""
     return f"https://abs.twimg.com/responsive-web/client-web/{k}.{v}.js"
 
 
-def get_scripts():
-    cache_dir = "/tmp/twscrape-ops"
+def get_scripts() -> list[str]:
+    """Return the list of scripts from the given URL."""
+    cache_dir = os.path.join("/tmp", "twscrape-ops")
     os.makedirs(cache_dir, exist_ok=True)
 
-    rep = client.get("https://twitter.com/elonmusk")
-    rep.raise_for_status()
-    urls = []
+    with ExitStack() as stack:
+        client = stack.enter_context(httpx.Client(
+            headers={"user-agent": fake_useragent.UserAgent().chrome},
+            limits=Limits(timeout=10.0),
+        ))
 
-    scripts = rep.text.split('e=>e+"."+')[1].split('[e]+"a.js"')[0]
-    try:
-        for k, v in json.loads(scripts).items():
-            urls.append(script_url(k, f"{v}a"))
-    except json.decoder.JSONDecodeError as e:
-        print(scripts)
-        print(e)
-        exit(1)
+        response = client.get("https://twitter.com/elonmusk")
+        response.raise_for_status()
 
-    v = rep.text.split("/client-web/main.")[1].split(".")[0]
-    urls.append(script_url("main", v))
+        urls = []
+        scripts = response.text.split('e=>e+"."+')[1].split('[e]+"a.js"')[0]
+        try:
+            for k, v in json.loads(scripts).items():
+                urls.append(script_url(k, f"{v}a"))
+        except json.JSONDecodeError as e:
+            print(scripts)
+            print(e)
+            exit(1)
 
-    urls = [
-        x
-        for x in urls
-        if "/i18n/" not in x and "/icons/" not in x and "react-syntax-highlighter" not in x
-    ]
+        v = response.text.split("/client-web/main.")[1].split(".")[0]
+        urls.append(script_url("main", v))
 
-    scripts = []
-    for i, x in enumerate(urls, 1):
-        cache_path = os.path.join(cache_dir, x.split("/")[-1].split("?")[0])
-        if os.path.exists(cache_path):
-            with open(cache_path) as fp:
-                scripts.append(fp.read())
-            continue
+        urls = [
+            x
+            for x in urls
+            if "/i18n/" not in x and "/icons/" not in x and "react-syntax-highlighter" not in x
+        ]
 
-        print(f"({i:3d} / {len(urls):3d}) {x}")
-        rep = client.get(x)
-        rep.raise_for_status()
+        for i, x in enumerate(urls, 1):
+            cache_path = os.path.join(cache_dir, urlparse(x).path.split("/")[-1])
+            if os.path.exists(cache_path):
+                with open(cache_path) as fp:
+                    urls[i - 1] = fp.read()
+                continue
 
-        with open(cache_path, "w") as fp:
-            fp.write(rep.text)
-        scripts.append(rep.text)
+            print(f"({i:3d} / {len(urls):3d}) {x}")
+            response = client.get(x)
+            response.raise_for_status()
 
-    return scripts
+            with open(cache_path, "w") as fp:
+                fp.write(response.text)
+            urls[i - 1] = response.text
 
-
-all_pairs = {}
-for txt in get_scripts():
-    pairs = re.findall(r'queryId:"(.+?)".+?operationName:"(.+?)"', txt)
-    pairs = {op_name: op_id for op_id, op_name in pairs}
-
-    for k, v in pairs.items():
-        if k in all_pairs and v != all_pairs[k]:
-            print(f"DIFF: {k} = {v} != {all_pairs[k]}")
-
-        all_pairs[k] = v
+        return urls
 
 
-for k, v in all_pairs.items():
-    print(f'OP_{k} = "{v}/{k}"')
+def extract_pairs(scripts: list[str]) -> dict[str, str]:
+    """Return the dictionary of operation names and query IDs."""
+    all_pairs = collections.defaultdict(str)
+    for txt in scripts:
+        for match in REGEX_PATTERN.findall(txt):
+            query_id, operation_name = match
+            all_pairs[operation_name] = query_id
 
-print("-" * 40)
+    return dict(all_pairs)
 
-for x in ops:
-    print(f'OP_{x} = "{all_pairs.get(x, "???")}/{x}"')
+
+def print_pairs(all_pairs: dict[str, str]):
+    """Print the pairs of operation names and query IDs."""
+    for k, v in all_pairs.items():
+        print(f'OP_{k} = "{v}/{k}"')
+
+    print("-" * 40)
+
+    for x in ops:
+        print(f'OP_{x} = "{all_pairs.get(x, "???")}/{x}"')
+
+
+if __name__ == "__main__":
+    ops = [x.strip() for x in open("./twscrape/api.py").read().split("\n")]
+    ops = [x.split("=")[0].removeprefix("OP_").strip() for x in ops if x.startswith("OP_")]
+
+    scripts = get_scripts()
+    all_pairs = extract_pairs(scripts)
+    print_pairs(all_pairs)
