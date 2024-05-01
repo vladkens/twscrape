6b7ca8d56839b84aa67182ab35fa69d1aafd092e
crisnelio551 <168581237+crisnelio551@users.noreply.github.com>
2024-05-01 19:14:57 +0000

    improve

--- twscrape/logger.py
+++ twscrape/logger.py
@@ -1,5 +1,18 @@
 import sys
 from typing import Literal
-
 from loguru import logger
 
+def set\_logger(log\_level: Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'] = 'INFO') -> None:
+"""
+Sets up the logger with the specified log level.
+
+:param log\_level: The log level to use. Default is 'INFO'.
+:type log\_level: Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+"""
+logger.remove()
+logger.add(sys.stderr, level=log\_level)
+
+set\_logger()
+
+logger.info('This is an info message')
+logger.error('This is an error message')
