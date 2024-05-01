66fe9c228b1a37be149879cb4e412e1b3165fb19
crisnelio551 <168581237+crisnelio551@users.noreply.github.com>
2024-05-01 19:14:47 +0000

    improve

--- tests/test_queue_client.py
+++ tests/test_queue_client.py
@@ -14,110 +14,100 @@ URL = "https://example.com/api"
 CF = tuple[AccountsPool, QueueClient]
 
 
-async def get_locked(pool: AccountsPool) -> set[str]:
-    rep = await pool.get_all()
-    return set([x.username for x in rep if x.locks.get("SearchTimeline", None) is not None])
+async def get_locked_accounts(pool: AccountsPool) -> set[str]:
+    return {x.username for x in await pool.get_all() if x.locks.get("SearchTimeline", None) is not None}
 
 
 async def test_lock_account_when_used(httpx_mock: HTTPXMock, client_fixture):
     pool, client = client_fixture
 
-    locked = await get_locked(pool)
-    assert len(locked) == 0
+    locked_before = await get_locked_accounts(pool)
+    assert not locked_before
 
     # should lock account on getting it
-    await client.__aenter__()
-    locked = await get_locked(pool)
-    assert len(locked) == 1
-    assert "user1" in locked
+    async with client:
+        locked_after = await get_locked_accounts(pool)
+        assert len(locked_after) == 1
+        assert "user1" in locked_after
 
     # keep locked on request
     httpx_mock.add_response(url=URL, json={"foo": "bar"}, status_code=200)
-    assert (await client.get(URL)).json() == {"foo": "bar"}
+    response = await client.get(URL)
+    assert response.json() == {"foo": "bar"}
 
-    locked = await get_locked(pool)
-    assert len(locked) == 1
-    assert "user1" in locked
+    locked_after = await get_locked_accounts(pool)
+    assert len(locked_after) == 1
+    assert "user1" in locked_after
 
     # unlock on exit
-    await client.__aexit__(None, None, None)
-    locked = await get_locked(pool)
-    assert len(locked) == 0
-
 
 async def test_do_not_switch_account_on_200(httpx_mock: HTTPXMock, client_fixture: CF):
     pool, client = client_fixture
 
     # get account and lock it
-    await client.__aenter__()
-    locked1 = await get_locked(pool)
-    assert len(locked1) == 1
+    async with client:
+        locked_accounts_before = await get_locked_accounts(pool)
+        assert len(locked_accounts_before) == 1
 
-    # make several requests with status=200
-    for x in range(1):
-        httpx_mock.add_response(url=URL, json={"foo": x}, status_code=200)
-        rep = await client.get(URL)
-        assert rep is not None
-        assert rep.json() == {"foo": x}
+        # make several requests with status=200
+        for x in range(1):
+            httpx_mock.add_response(url=URL, json={"foo": x}, status_code=200)
+            response = await client.get(URL)
+            assert response is not None
+            assert response.json() == {"foo": x}
 
-    # account should not be switched
-    locked2 = await get_locked(pool)
-    assert locked1 == locked2
-
-    # unlock on exit
-    await client.__aexit__(None, None, None)
-    locked3 = await get_locked(pool)
-    assert len(locked3) == 0
+        # account should not be switched
+        locked_accounts_after = await get_locked_accounts(pool)
+        assert locked_accounts_before == locked_accounts_after
 
 
 async def test_switch_acc_on_http_error(httpx_mock: HTTPXMock, client_fixture: CF):
     pool, client = client_fixture
 
     # locked account on enter
-    await client.__aenter__()
-    locked1 = await get_locked(pool)
-    assert len(locked1) == 1
+    async with client:
+        locked_accounts_before = await get_locked_accounts(pool)
+        assert len(locked_accounts_before) == 1
 
-    # fail one request, account should be switched
-    httpx_mock.add_response(url=URL, json={"foo": "1"}, status_code=403)
-    httpx_mock.add_response(url=URL, json={"foo": "2"}, status_code=200)
+        # fail one request, account should be switched
+        httpx_mock.add_response(url=URL, json={"foo": "1"}, status_code=403)
+        httpx_mock.add_response(url=URL, json={"foo": "2"}, status_code=200)
 
-    rep = await client.get(URL)
-    assert rep is not None
-    assert rep.json() == {"foo": "2"}
+        response = await client.get(URL)
+        assert response is not None
+        assert response.json() == {"foo": "2"}
 
-    locked2 = await get_locked(pool)
-    assert len(locked2) == 2
+        locked_accounts_after = await get_locked_accounts(pool)
+        assert len(locked_accounts_after) == 2
 
     # unlock on exit (failed account still should locked)
-    await client.__aexit__(None, None, None)
-    locked3 = await get_locked(pool)
-    assert len(locked3) == 1
-    assert locked1 == locked3  # failed account locked
+    locked_accounts_after_exit = await get_locked_accounts(pool)
+    assert len(locked_accounts_after_exit) == 1
+    assert locked_accounts_before == locked_accounts_after_exit  # failed account locked
 
 
 async def test_retry_with_same_acc_on_network_error(httpx_mock: HTTPXMock, client_fixture: CF):
     pool, client = client_fixture
 
     # locked account on enter
-    await client.__aenter__()
-    locked1 = await get_locked(pool)
-    assert len(locked1) == 1
+    async with client:
+        locked_accounts_before = await get_locked_accounts(pool)
+        assert len(locked_accounts_before) == 1
 
-    # timeout on first request, account should not be switched
-    httpx_mock.add_exception(httpx.ReadTimeout("Unable to read within timeout"))
-    httpx_mock.add_response(url=URL, json={"foo": "2"}, status_code=200)
+        # timeout on first request, account should not be switched
+        httpx_mock.add_exception(httpx.ReadTimeout("Unable to read within timeout"))
+        httpx_mock.add_response(url=URL, json={"foo": "2"}, status_code=200)
 
-    rep = await client.get(URL)
-    assert rep is not None
-    assert rep.json() == {"foo": "2"}
+        response = await client.get(URL)
+        assert response is not None
+        assert response.json() == {"foo": "2"}
 
-    locked2 = await get_locked(pool)
-    assert locked2 == locked1
+        locked_accounts_after = await get_locked_accounts(pool)
+        assert locked_accounts_after == locked_accounts_before
 
-    # check username added to request obj (for logging)
-    username = getattr(rep, "__username", None)
-    assert username is not None
+        # check username added to request obj (for logging)
+        username = getattr(response, "__username", None)
+        assert username is not None
 
 
 async def test_ctx_closed_on_break(httpx_mock: HTTPXMock, client_fixture: CF):
@@ -137,16 +127,16 @@ async def test_ctx_closed_on_break(httpx_mock: HTTPXMock, client_fixture: CF):
                 else:
                     httpx_mock.add_response(url=URL, json={"counter": counter}, status_code=200)
 
-                rep = await c.get(URL)
+                response = await c.get(URL)
 
                 if check_retry:
                     assert before_ctx != c.ctx
                 elif before_ctx is not None:
                     assert before_ctx == c.ctx
 
-                assert rep is not None
-                assert rep.json() == {"counter": counter}
-                yield rep.json()["counter"]
+                assert response is not None
+                assert response.json() == {"counter": counter}
+                yield response.json()["counter"]
 
                 if counter == 9:
                     return
