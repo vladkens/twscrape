4f727ef084619f16e9cf6896ff042575f2c1e2ee
crisnelio551 <168581237+crisnelio551@users.noreply.github.com>
2024-05-01 19:14:52 +0000

    improve

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,23 +1,8 @@
+import asyncio
 import json
 import os
 from pathlib import Path
 from typing import (
     Any,
     AsyncContextManager,
-    Callable,
-    List,
-    Optional,
-)
-
-import aiofiles
-import typer
-from twscrape import API, gather, parse_tweet
-from twscrape.logger import set_log_level
-from twscrape.models import PollCard, SummaryCard, Tweet, User, UserRef
-
-BASE_DIR = Path(__file__).parent
-DATA_DIR = BASE_DIR / "mocked-data"
-os.makedirs(DATA_DIR, exist_ok=True)
-
-set_log_level("DEBUG")
 
